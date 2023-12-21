484df89db4b1b1ee38edef1701c5103fff96bf79
njtran <nichotr@amazon.com>
2023-12-21 14:44:55 -0800

    update label

--- pkg/model/node.go
+++ pkg/model/node.go
@@ -288,7 +288,7 @@ func (n *Node) HasPrice() bool {
 func (n *Node) UpdatePrice(pricing *pricing.Provider) {
 	// If the node has the instance-price override label, don't look up pricing
 	// and use the value here.
-	if val, ok := n.node.Labels["instance-price"]; ok {
+	if val, ok := n.node.Labels["eks-node-viewer/instance-price"]; ok {
 		if price, err := strconv.ParseFloat(val, 64); err == nil {
 			n.Price = price
 			return
