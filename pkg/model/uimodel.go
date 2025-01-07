507e7c853713ea02d6b6323d7b4a3251b0e8774c
Ryo Nakamine <rnakamine8080@gmail.com>
2025-01-07 16:13:41 +0900

    ComputeLabel to return '-' instead of labelName

--- pkg/model/uimodel.go
+++ pkg/model/uimodel.go
@@ -183,9 +183,6 @@ func (u *UIModel) writeNodeInfo(n *Node, w io.Writer, resources []v1.ResourceNam
 				if !ok {
 					// support computed label values
 					labelValue = n.ComputeLabel(label)
-					if labelValue == label {
-						labelValue = "-"
-					}
 				}
 				fmt.Fprintf(w, "\t%s", labelValue)
 			}
